dc83261d28830ffaeb4355de9127b8048b9d4c14
Isaac Ezer <isaac.ezer@gmail.com>
2013-07-06 17:15:16 -0400

    Revert "use templates to show different lists of TODOs, including just Active ones"
    This reverts commit ccaac657616a7d284ea8593af62630a2477701b6.

--- ember/todomvc/js/router.js
+++ ember/todomvc/js/router.js
@@ -1,23 +1,9 @@
 Todos.Router.map(function () {
-  this.resource('todos', { path: '/' }, function () {
-		// additional child routes    
-		+    this.route('active');
-  });
+  this.resource('todos', { path: '/' });
 });
 
-Todos.TodosIndexRoute = Ember.Route.extend({
+Todos.TodosRoute = Ember.Route.extend({
   model: function () {
     return Todos.Todo.find();
-  }
-});
-
-Todos.TodosActiveRoute = Ember.Route.extend({
-  model: function(){
-    return Todos.Todo.filter(function (todo) {
-      if (!todo.get('isCompleted')) { return true; }
-    });
-  },
-  renderTemplate: function(controller){
-    this.render('todos/index', {controller: controller});
   }
 });
