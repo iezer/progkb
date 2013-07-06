1713b2ba6f25619516e1ad3dc9b958592382eafd
Isaac Ezer <isaac.ezer@gmail.com>
2013-07-06 17:22:46 -0400

    show only active todos

--- ember/todomvc/js/router.js
+++ ember/todomvc/js/router.js
@@ -15,3 +15,23 @@ Todos.TodosIndexRoute = Ember.Route.extend({
     return Todos.Todo.find();
   }
 });
+
+Todos.Router.map(function () {
+  this.resource('todos', { path: '/' }, function () {
+    // additional child routes    
+    this.route('active');
+  });
+});
+
+// ... additional lines truncated for brevity ...
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return Todos.Todo.filter(function (todo) {
+      if (!todo.get('isCompleted')) { return true; }
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
+  }
+});
