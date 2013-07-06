ccaac657616a7d284ea8593af62630a2477701b6
Isaac Ezer <isaac.ezer@gmail.com>
2013-07-06 16:40:13 -0400

    use templates to show different lists of TODOs, including just Active ones

--- ember/todomvc/js/router.js
+++ ember/todomvc/js/router.js
@@ -1,9 +1,23 @@
 Todos.Router.map(function () {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+		// additional child routes    
+		+    this.route('active');
+  });
 });
 
-Todos.TodosRoute = Ember.Route.extend({
+Todos.TodosIndexRoute = Ember.Route.extend({
   model: function () {
     return Todos.Todo.find();
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return Todos.Todo.filter(function (todo) {
+      if (!todo.get('isCompleted')) { return true; }
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
   }
 });
