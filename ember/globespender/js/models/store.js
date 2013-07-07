7d5e50e8980022b604c9ad73531a04ac227ab5a3
Isaac Ezer <isaac.ezer@gmail.com>
2013-07-06 21:17:40 -0400

    add local storage

--- ember/globespender/js/models/store.js
+++ ember/globespender/js/models/store.js
@@ -1,4 +1,8 @@
 Expenses.Store = DS.Store.extend({
   revision: 12,
-  adapter: 'DS.FixtureAdapter'
+  adapter: 'Expenses.LSAdapter'
+});
+
+Expenses.LSAdapter = DS.LSAdapter.extend({
+  namespace: 'expenses-emberjs'
 });
