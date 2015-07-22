2207cdc519daf3acae029d24e26581ed8b80e45f
Isaac Ezer <iezer@iezerMBP.local>
2015-07-22 10:10:22 -0700

    git setup scripts

---
+++ config/git-setup.sh
@@ -0,0 +1,8 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global core.editor emacs
+git config --global core.excludesfile ./.gitignore
+git config --global user.name "Isaac Ezer"
+git config --global user.email isaac.ezer@gmail.com
