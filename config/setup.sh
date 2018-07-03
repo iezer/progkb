f146e9c537877aaf07b57ef15864496d6e4b0b1f
Isaac Ezer <isaac.ezer@gmail.com>
2018-07-03 11:16:45 +0200

    Update setup.sh with full paths

--- config/setup.sh
+++ config/setup.sh
@@ -1,8 +1,9 @@
-ln -s ./.emacs ~/.emacs
-ln -s ./.emacs.d ~/.emacs.d
-ln -s ./.bashrc ~/.bashrc
-ln -s ./.bash_profile ~/.bash_profile
-ln -s ./elisp ~/elisp
-ln -s ./.nvmrc ~/.nvmrc
-ln -s ./.gitignore ~/.gitignore
+ln -s ~/Code/progkb/config/.emacs ~/.emacs
+ln -s ~/Code/progkb/config/.emacs.d ~/.emacs.d
+ln -s ~/Code/progkb/config/.bashrc ~/.bashrc
+ln -s ~/Code/progkb/config/.bash_profile ~/.bash_profile
+ln -s ~/Code/progkb/config/elisp ~/elisp
+ln -s ~/Code/progkb/config/.nvmrc ~/.nvmrc
+ln -s ~/Code/progkb/config/.gitignore ~/.gitignore
+ln -s ~/Code/progkb/config/.gitconfig ~/.gitconfig
 git config --global core.excludesfile '~/.gitignore'
